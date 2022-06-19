e9a61ad6c615833a1f97e80b96b6143059099f5d
Emilio <97320637+emiliozpt@users.noreply.github.com>
2022-06-19 11:02:50 -0500

    Landing page updates

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import FeatureBig from '../components/FeatureBig'
 import CTA from '../components/CTA'
 import FAQ from '../components/FAQ'
 import Footer from '../components/Footer'
+import Pricing from '../components/Pricing'
 
 export default function Home() {
   return (
@@ -44,6 +45,7 @@ export default function Home() {
         inverted
       />
       <CTA />
+      <Pricing />
       <FAQ />
       <Footer />
     </div>
