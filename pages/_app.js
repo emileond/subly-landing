37389958a15ae1a7b417d8c6b568a149e6f2bb34
Emilio <sonarart@gmail.com>
2023-01-02 22:03:39 -0600

    adding rewardful

--- pages/_app.js
+++ pages/_app.js
@@ -13,9 +13,31 @@ function MyApp({ Component, pageProps }) {
       offset: 100,
     })
   }, [])
+
+  useEffect(() => {
+    ;(function (w, r) {
+      w._rwq = r
+      w[r] =
+        w[r] ||
+        function () {
+          ;(w[r].q = w[r].q || []).push(arguments)
+        }
+    })(window, 'rewardful')
+
+    let s = document.createElement('script')
+    s.async = true
+    s.src = 'https://r.wdfl.co/rw.js'
+    s.setAttribute('data-rewardful', 'c5808b')
+    document.head.appendChild(s)
+    rewardful('ready', function () {
+      console.log('Rewardful Ready!')
+    })
+  }, [])
+
   splitbee.init({
     token: 'SLLB2C20R4FI',
   })
+
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
